e3589b2109f0eef02de2df37c05c3462a897d3a2
vimukthioshada <77786139+vimukthioshada@users.noreply.github.com>
2024-01-16 21:17:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const playlistId = '4REFRayEtcqlf4EIzXVmFi';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/4REFRayEtcqlf4EIzXVmFi?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/> 
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
